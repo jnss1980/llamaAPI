00534ed1a8e336dfd03c1e56ea69f3dba8709e08
liruochen <jnss1980@gmail.com>
2023-09-10 01:58:58 +0800

    OK

---
+++ src/main/java/com/example/llama/llamaapi2/LlamaApi2Application.java
@@ -0,0 +1,13 @@
+package com.example.llama.llamaapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlamaApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlamaApi2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/llama/llamaapi2/LlamaApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.llama.llamaapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlamaApi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
